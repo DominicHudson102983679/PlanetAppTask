5bb13f4e0cd3cc8eb94cf29b4744ec1bcf376696
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-08-27 11:09:51 +1000

    component setup

--- planet-app/src/app/app-routing.module.ts
+++ planet-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PlanetDetailComponent } from './planet-detail/planet-detail.component'
+import { PlanetsCreateComponent } from './planets-create/planets-create.component'
+import { PlanetsListComponent } from './planets-list/planets-list.component'
+
+const routes: Routes = [
+  { path: 'planet-detail', component:PlanetDetailComponent},
+  { path: 'planets-create', component:PlanetsCreateComponent},
+  { path: 'planets-list', component:PlanetsListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- planet-app/src/app/app.module.ts
+++ planet-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetsListComponent } from './planets-list/planets-list.component';
+import { PlanetDetailComponent } from './planet-detail/planet-detail.component';
+import { PlanetsCreateComponent } from './planets-create/planets-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsListComponent,
+    PlanetDetailComponent,
+    PlanetsCreateComponent
   ],
   imports: [
     BrowserModule,
